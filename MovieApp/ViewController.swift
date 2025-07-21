639931c568aefbfce9c809ec2134ff39b934d729
Mohamed Kotb <kotbm055@gmail.com>
2025-07-22 00:48:41 +0300

    Initial Commit

---
+++ MovieApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieApp
+//
+//  Created by Mohamed Kotb on 22/07/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
